9029c1ba4924cf315101c3c4665ee1a4d5a81c92
Farzad <amirifarzad1381@gmail.com>
2025-11-20 20:34:10 +0330

    base_model and domain layer

---
+++ src/domain/model/user.go
@@ -0,0 +1,24 @@
+package model
+
+type User struct {
+	BaseModel
+	Name      string
+	Email     string
+	Password  string
+	Enabled   bool
+	UserRoles *[]UserRoles
+}
+
+type Role struct {
+	BaseModel
+	Name      string
+	UserRoles *[]UserRoles
+}
+
+type UserRoles struct {
+	BaseModel
+	User   User
+	Role   Role
+	UserId int
+	RoleId int
+}
